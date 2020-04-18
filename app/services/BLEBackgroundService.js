03c6206fac40dc5eaa22aa3f842480cac89626d2
Vitor Pamplona <vitor@vitorpamplona.com>
2020-04-18 18:58:14 -0400

    Simplifying logs.

--- app/services/BLEBackgroundService.js
+++ app/services/BLEBackgroundService.js
@@ -10,36 +10,24 @@ export default class BLEBackgroundService {
     static start(uuid) {
       console.log(uuid, "Starting Advertising");
       BLEAdvertiser.broadcast(uuid, [12,23,56], {})
-      .then((sucess) => {
-        console.log(uuid, "Adv Successful", sucess);
-      }).catch(error => {
-        console.log(uuid, "Adv Error", error); 
-      });
+      .then(sucess => console.log(uuid, "Adv Successful", sucess))
+      .catch(error => console.log(uuid, "Adv Error", error));
       
       console.log(uuid, "Starting Scanner");
       BLEAdvertiser.scan([12,23,56], {})
-      .then((sucess) => {
-        console.log(uuid, "Scan Successful", sucess);
-      }).catch(error => {
-        console.log(uuid, "Scan Error", error); 
-      });
     }
+      .then(sucess => console.log(uuid, "Scan Successful", sucess))
+      .catch(error => console.log(uuid, "Scan Error", error)); 
 
     static stop(uuid){
       console.log(uuid, "Stopping Broadcast");
       BLEAdvertiser.stopBroadcast()
-        .then((sucess) => {
-          console.log(uuid, "Stop Broadcast Successful", sucess);
-        }).catch(error => {
-          console.log(uuid, "Stop Broadcast Error", error); 
-        });
+        .then(sucess => console.log(uuid, "Stop Broadcast Successful", sucess))
+        .catch(error => console.log(uuid, "Stop Broadcast Error", error));
 
       console.log(uuid, "Stopping Scanning");
       BLEAdvertiser.stopScan()
-        .then((sucess) => {
-          console.log(uuid, "Stop Scan Successful", sucess);
-        }).catch(error => {
-          console.log(uuid, "Stop Scan Error", error); 
-        });
     }
+        .then(sucess => console.log(uuid, "Stop Scan Successful", sucess))
+        .catch(error => console.log(uuid, "Stop Scan Error", error));
 }

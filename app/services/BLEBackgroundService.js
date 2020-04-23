c956799c44aa693ac38f07872018216839263efb
Vitor Pamplona <vitor@vitorpamplona.com>
2020-04-23 13:54:56 -0400

    New version of the BLE Library and added optional parameters.

--- app/services/BLEBackgroundService.js
+++ app/services/BLEBackgroundService.js
@@ -135,12 +135,17 @@ export default class BLEBackgroundService {
     AsyncStorage.getItem(MY_UUID).then(uuid => {
       if (uuid) {
         console.log(uuid, "Starting Advertising");
-        BLEAdvertiser.broadcast(uuid, [12,23,56], {})
+        BLEAdvertiser.broadcast(uuid, [12,23,56], {
+          advertiseMode: BLEAdvertiser.ADVERTISE_MODE_LOW_POWER, 
+          txPowerLevel: BLEAdvertiser.ADVERTISE_TX_POWER_LOW, 
+          connectable: false, 
+          includeDeviceName: false, includeTxPowerLevel: false
+        })
         .then(sucess => this.emitBroadcastingStatus("Started"))
         .catch(error => this.emitBroadcastingStatus(error));
         
         console.log(uuid, "Starting Scanner");
-        BLEAdvertiser.scan([12,23,56], {})
+        BLEAdvertiser.scan([12,23,56], {scanMode: BLEAdvertiser.SCAN_MODE_BALANCED})
         .then(sucess => this.emitScanningStatus("Started"))
         .catch(error => this.emitScanningStatus(error)); 
       }

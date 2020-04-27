ebf3328f01b541c893f9ec63e99058d22b0f85b8
Vitor Pamplona <vitor@vitorpamplona.com>
2020-04-27 14:13:01 -0400

    Adding Bulk upload of up to 10000 records at every 15 mins

--- app/helpers/SyncDB.js
+++ app/helpers/SyncDB.js
@@ -6,55 +6,57 @@ import AsyncStorage from '@react-native-community/async-storage';
 //export const SERVER = "192.168.0.11:4567";
 export const SERVER = "safepath.tch.harvard.edu:80";
 
+const ACCEPT_JSON = { "Accept": "application/json", "Content-Type": "application/json" };
+
 export async function saveContactToUpload(_uploader, _contact, _rssi, _date) {
   let contactData = { uploader:_uploader, contact: _contact, rssi:_rssi, date:_date.toISOString() };
   AsyncStorage.setItem(CONTACT + _contact + _date.toISOString(), JSON.stringify(contactData));  
 };
 
-function uploadAndRemove(key) {
-    AsyncStorage.getItem(key).then(contact => {
-        if (contact) {
-            fetch('http://' + SERVER + "/api/v1/contacts", 
-                {
-                    method: "POST",
-                    headers: {
-                    "Accept": "application/json",
-                    "Content-Type": "application/json"
-                    },
-                    body: contact
-                }
-            ).then(response => {
-                if (response.status == 200) {
-                    AsyncStorage.removeItem(key);
-                }
-            }).catch(error => {
-                console.log("Upload Error", error); 
-            });
-        } 
+export async function readyToUpload() {
+    let ks = await AsyncStorage.getAllKeys();
+    return ks.filter(key => key.startsWith(CONTACT)).length;
+}
+
+async function upload(contact_list) {
+    return fetch('http://' + SERVER + "/api/v1/contacts", 
+        {
+            method: "POST",
+            headers: ACCEPT_JSON,
+            body: JSON.stringify(values)
+        }
+    );
+}
+
+function upload1000Keys() {
+    AsyncStorage.getAllKeys().then(ks => {
+        let keysToUpload = ks.sort().filter(key => key.startsWith(CONTACT)).slice(0,10000);
+
+        AsyncStorage.multiGet(keysToUpload).then(data => {
+            if (data.length > 0) { 
+                keys = data.map(keyValue => keyValue[0]);
+                values = data.map(keyValue => JSON.parse(keyValue[1]));
+                
+                upload(values).then(response => {
+                    if (response.status == 200) {
+                        AsyncStorage.multiRemove(keys);
+                    }
+                }).catch(error => {
+                    console.log("Upload Error", error); 
+                });
+            }
+        });
     });
 }
 
-export async function sync () {
-    fetch('http://' + SERVER + "/api/v1/health", 
-        {
-            method: "GET",
-            headers: {
-            "Accept": "application/json",
-            "Content-Type": "application/json"
-            }
-        }
-    ).then(response => {
+async function isOnline() {
+    return fetch('http://' + SERVER + "/api/v1/health", { method: "GET", headers: ACCEPT_JSON } );
+}
 
+export async function sync () {
+    isOnline().then(response => {
         if (response.status == 200) { // is online
-            AsyncStorage.getAllKeys().then(ks => {
-                ks.sort().map(key => {
-                    // it's a uuid
-                    if (key.startsWith(CONTACT)) {
-                        console.log("Uploading Key", key);
-                        uploadAndRemove(key);
-                    }
-                })
-            });
+            upload1000Keys();
         }
     }).catch(error => {
         console.log("Phone Offline", error); 

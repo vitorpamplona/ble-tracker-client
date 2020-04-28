507540703c753581a8fe943099c433bf6eb49a1f
Vitor Pamplona <vitor@vitorpamplona.com>
2020-04-28 15:00:00 -0400

    Finishing setup for Background Fetch on iOS

--- ios/BCHContactTracing/AppDelegate.m
+++ ios/BCHContactTracing/AppDelegate.m
@@ -10,6 +10,7 @@
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+#import <TSBackgroundFetch/TSBackgroundFetch.h>
 
 @implementation AppDelegate
 
@@ -27,6 +28,10 @@
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+
+  // [REQUIRED] Register BackgroundFetch
+  [[TSBackgroundFetch sharedInstance] didFinishLaunching];
+
   return YES;
 }
 
